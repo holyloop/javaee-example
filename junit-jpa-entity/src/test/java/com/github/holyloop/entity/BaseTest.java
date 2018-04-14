208ac0facbc3a5480755e9fcc94aa2b497022232
holyloop <holyloop@163.com>
2018-04-14 16:27:34 +0800

    complete basic test demo

---
+++ junit-jpa-entity/src/test/java/com/github/holyloop/entity/BaseTest.java
@@ -0,0 +1,59 @@
+package com.github.holyloop.entity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.h2.tools.RunScript;
+import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class BaseTest {
+
+    protected static EntityManagerFactory emf;
+    protected static EntityManager em;
+
+    @BeforeClass
+    public static void init() {
+        emf = Persistence.createEntityManagerFactory("test");
+        em = emf.createEntityManager();
+    }
+
+    @Before
+    public void initDB() {
+        Session session = em.unwrap(Session.class);
+        session.doWork(new Work() {
+            @Override
+            public void execute(Connection connection) throws SQLException {
+                try {
+                    File script = new File(getClass().getResource("/data.sql").getFile());
+                    RunScript.execute(connection, new FileReader(script));
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException("could not initialize with script");
+                }
+            }
+        });
+    }
+
+    @After
+    public void clean() {
+        em.clear();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        em.clear();
+        em.close();
+        emf.close();
+    }
+}
---
+++ junit-jpa-entity/src/test/resources/data.sql
@@ -0,0 +1,2 @@
+DELETE FROM book;
+INSERT INTO book(id, title, author) VALUES (1, 'Spring in Action', 'Craig Walls');
